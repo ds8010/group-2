1c34d6daf0e92500d7c7d0c61c4c5363063a5242
ds8010 <156882507+ds8010@users.noreply.github.com>
2024-02-07 23:38:08 +0400

    version2

--- Group2_Activity1.py
+++ Group2_Activity1.py
@@ -1,38 +1,26 @@
-import turtle
+from turtle import Turtle, Screen
 
-def setPos(x,y):
-    turtle.setheading(headin0)
-    turtle.penup()
-    turtle.goto(x,y)
-    turtle.pendown()
 
-def pattern(hex_color,circle_color,square_color,border_color):
-    x=-250
-    y=150
-    # The first row (hexagon, circle, square)
-    setPos(x,y) # the turtle is positioned at (-150,150) on the xy coordinate with heading 0 (to the east)
-    hexagon(50,border_color,hex_color) # starting from (-150,150) it draws hexagon with side_length of 50 and the turtle ends at its starting point (-150,150)
-    setPos(x+150,y) # this takes the turtle to (0,150)
-    circle(50,border_color,circle_color)
-    setPos(x+225,y)
-    square(90,border_color,square_color)
-    
-    # The second row (hexagon, circle, square)
-    setPos(x+115,y-120)
-    hexagon(50,border_color,hex_color)
-    setPos(x+265,y-120)
-    circle(50,border_color,circle_color)
-    setPos(x+340,y-120)
-    square(90,border_color,square_color)
-    
-    # The third row (hexagon, circle, square)
-    setPos(x+230,y-240)
-    hexagon(50,border_color,hex_color)
-    setPos(x+380,y-240)
-    circle(50,border_color,circle_color)
-    setPos(x+455,y-240)
-    square(90,border_color,square_color)
+def pattern(turta,x,y,hexa_color, circle_color, square_color, border_color):
+    """Creates a row of the pattrn in the order hexagon, circle and square from a given x and y coordinate."""
+    setPos(turta,x,y)
+    hexagon(turta,50,border_color,hexa_color)
+    setPos(turta,x+150,y)
+    circle(turta,50,border_color,circle_color)
+    setPos(turta,x+225,y)
+    square(turta,90,border_color,square_color)
 
 def main():
+    Hexa_color = input('Enter the color what you want to fill the hexagon with: ')
+    Circle_color = input('Enter the color what you want to fill the circle with: ')
+    Square_color = input('Enter the color what you want to fill the square with: ')
+    Border_color = input('Enter the color what you want for the border of the shapes: ') 
+    turta = Turtle()
+    win = Screen()
+    turta.speed(0)
+    pattern(turta,-150,150,Hexa_color, Circle_color, Square_color,Border_color)
+    pattern(turta,-50,20, Hexa_color,Circle_color,Square_color,Border_color)
+    pattern(turta,50,-110,Hexa_color,Circle_color,Square_color,Border_color)
+    win.exitonclick()
 
 main()
