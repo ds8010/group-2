4ce9985a11e4b6bf6b0c431b0cb19d903026aaf1
Yosef <yms9866@rit.edu>
2024-02-09 21:16:18 +0400

    Final Version

--- Group2_Activity1.py
+++ Group2_Activity1.py
@@ -1,6 +1,6 @@
 """Group: 2
    Author1:Yosef Shibele
-   Part: Contributed with the "pattern" function and the "main" function and also add "go_to_east" function to make the pattern function more better.
+   Part: Contributed with the "pattern" function and the "main" function and also add "go_to_east" function to make the pattern function more better. And helped with the comments.
 
    Author2:Hamad Alsaadi 
    Part: Contibuted with the "square" function and the "setPos" function, also the comments for the 2 functions.
@@ -8,12 +8,18 @@
    Author3:Dona Pal
    Part: Contributed with the "hexagon" and "circle" functions. Also helped with the comments. 
 
-   Manifesto: This is a program is written to draw a hexagon, circle and square in a pattern in a row; and repeat the pattern two more times at different positions. """
+   Visit our github group repository: https://github.com/ds8010/group-2
+
+   Manifesto: This is a program is written to draw a hexagon, circle and square in a pattern in a row; and repeat the pattern two more times at different positions. 
+   
+   CODE COMPLETED"""
+
 
 from turtle import Turtle, Screen
 
 def hexagon(turta,side_length,border_color,hexa_color):
     """Draws hexagon with a given side_lengh and a given border_color and fills it with a given hex_color."""
+    
     turta.color(border_color)
     turta.fillcolor(hexa_color)
     turta.begin_fill()
@@ -69,21 +75,21 @@ def go_to_east(turta,distance):
     turta.forward(distance)
     turta.pendown()
 
-def pattern(turta, row_height, hexa_color, circle_color, square_color, border_color):
+def pattern(turta, row_height, hexa_color, circle_color, square_color, border_color): # by passing the row_heght parameter, this function is able to make the shapes with differnt size.
     """
-    row_height: is how big is the row. It is, actually, the diameter of the circle.
+    row_height: determines how wide is the row of the shapes. It is, actually, the diameter of the circle or the length of the square.
     
-    Draws a hexagon filled with  hexa_color color first followed by a circle filled with circle_color then followed by a square filled with square_color.
+    Draws a hexagon filled with  hexa_color color first followed by a circle filled with circle_color then followed by a square filled with square_color with a given row_heght
     
-    And the side_length of the hexagon and the radius of the circle are equal . And also the side_lengh of the square is equal to the side_length of the hexagon multiplied by 1.9. """
-    hex_side_length = 0.5*row_height
+     """
+    hex_side_length = (5/9)*row_height
     circle_radius = 0.5*row_height
-    square_side_length = hex_side_length*1.9
+    square_side_length = row_height
     
     hexagon(turta, hex_side_length, border_color, hexa_color)
     go_to_east(turta, 2.5*hex_side_length + 25)  # 25 is the gap between the hexagon and the circle
     circle(turta, circle_radius, border_color, circle_color)
-    go_to_east(turta,hex_side_length + 25) # 25 is the gap between the hexagon and the circle
+    go_to_east(turta, hex_side_length + 25) # 25 is the gap between the hexagon and the circle
     square(turta, square_side_length, border_color, square_color)
 
 def main():
@@ -95,12 +101,12 @@ def main():
     win = Screen()
     win.bgcolor("Skyblue")
     turta.speed(0)
-    setpos(turta,-250,150)
-    pattern(turta,100, Hexa_color, Circle_color, Square_color, Border_color)
-    setpos(turta,-150,30)
-    pattern(turta,100,Hexa_color, Circle_color, Square_color, Border_color)
-    setpos(turta,-50,-100)
-    pattern(turta,100,Hexa_color, Circle_color, Square_color, Border_color)
+    setpos(turta,-250,150) 
+    pattern(turta,90, Hexa_color, Circle_color, Square_color, Border_color) 
+    setpos(turta,-150,40)
+    pattern(turta,90,Hexa_color, Circle_color, Square_color, Border_color)
+    setpos(turta,-50,-70)
+    pattern(turta,90,Hexa_color, Circle_color, Square_color, Border_color)
     win.exitonclick()
 
 main()
