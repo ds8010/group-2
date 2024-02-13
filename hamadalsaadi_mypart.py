9b412fa844df7b148ff7c60d1d32fc927a2a81a6
hamad-alsaadi <158991079+hamad-alsaadi@users.noreply.github.com>
2024-02-13 13:00:17 +0400

    Create hamadalsaadi_mypart.py

---
+++ hamadalsaadi_mypart.py
@@ -0,0 +1,23 @@
+a_range=range(0,10)
+
+def whilevalues(r):
+    index = 0
+    while index < len(r):
+        print(r[index], end=" ")
+        index += 1
+
+whilevalues(a_range) 
+
+
+a_range=range(0,10)
+
+def forvalues(r):
+    """
+    Prints all the values in the given range on a single line using a for loop.
+    activity 4.3.5 which requires us to use a for loop in order to produce the same
+    as the older functions and print them on single lines.
+    """
+    for value in r:
+        print(value, end=" ")
+
+forvalues(a_range)
